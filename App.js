5fbc0b13c6b3e5c9158e4c072be878d4fd0c6cb2
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-12 18:06:15 -0300

    feat: expo dev client

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import "expo-dev-client";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleSignIn = () => {
+    const data = {
+      username,
+      email,
+      senha,
+    };
+    console.log(data);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bem-vindo(a)</Text>
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setUsername}
+        value={username}
+        placeholder="Seu nome"
+      />
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Digite seu Email"
+      />
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setSenha}
+        value={senha}
+        placeholder="Digite sua Senha"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonInput}>Acessar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +58,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F0F4FF",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 18,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 34,
+    alignSelf: "flex-start",
+    color: "#121212",
+    fontWeight: "bold",
+  },
+  textInput: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#fff",
+    marginBottom: 10,
+  },
+  button: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "blue",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonInput: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
